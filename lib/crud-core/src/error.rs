de093a0ee480ac2847dabdc0d9616692e069abdf
hayesHowYaDoin <jordanhayes98@gmail.com>
2025-08-10 22:03:56 +0000

    Made CRUDError implement std::error::Error

--- lib/crud-core/src/error.rs
+++ lib/crud-core/src/error.rs
@@ -1,3 +1,10 @@
-/// Errors that can occur in CRUD operations.
 #[derive(Debug)]
 pub struct CRUDError(pub String);
+
+impl std::fmt::Display for CRUDError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&self.0)
+    }
+}
+
+impl std::error::Error for CRUDError {}
